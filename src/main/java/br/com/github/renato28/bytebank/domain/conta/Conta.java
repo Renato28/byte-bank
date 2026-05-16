02cff15eb67446d2d3ea2998b7c4b36a139d7f37
Renato Nobrega <renatonobrega87@gmail.com>
2026-05-16 14:44:53 -0300

    implementando a classe conta

---
+++ src/main/java/br/com/github/renato28/bytebank/domain/conta/Conta.java
@@ -0,0 +1,76 @@
+package br.com.github.renato28.bytebank.domain.conta;
+
+import br.com.github.renato28.bytebank.domain.cliente.Cliente;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Conta {
+
+    private Integer numero;
+    private BigDecimal saldo;
+    private Cliente titular;
+
+    public Conta(Integer numero, Cliente titular) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = BigDecimal.ZERO;
+    }
+
+    public boolean possuiSaldo() {
+        return this.saldo.compareTo(BigDecimal.ZERO) != 0;
+    }
+
+    public void sacar(BigDecimal valor) {
+        this.saldo = this.saldo.subtract(valor);
+    }
+
+    public void depositar(BigDecimal valor) {
+        this.saldo = this.saldo.add(valor);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Conta conta = (Conta) o;
+        return Objects.equals(numero, conta.numero);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(numero);
+    }
+
+    @Override
+    public String toString() {
+        return "Conta{" +
+                "numero=" + numero +
+                ", saldo=" + saldo +
+                ", titular=" + titular +
+                '}';
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    public Cliente getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+}
