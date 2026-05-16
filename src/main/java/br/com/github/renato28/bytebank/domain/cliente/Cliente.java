826a4177ac56b3637b02a1d33610f7b74bbe8759
Renato Nobrega <renatonobrega87@gmail.com>
2026-05-16 14:37:20 -0300

    implementando a classe cliente

---
+++ src/main/java/br/com/github/renato28/bytebank/domain/cliente/Cliente.java
@@ -0,0 +1,61 @@
+package br.com.github.renato28.bytebank.domain.cliente;
+
+import java.util.Objects;
+
+public class Cliente {
+
+    private String nome;
+    private String cpf;
+    private String email;
+
+    public Cliente(DadosCadastroCliente dados) {
+        this.nome = dados.nome();
+        this.cpf = dados.cpf();
+        this.email = dados.email();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(cpf, cliente.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(cpf);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
