d84ec996b81a830f08d0af98cdc29b2277e7a0bf
Renato Nobrega <renatonobrega87@gmail.com>
2026-05-17 17:44:55 -0300

    implementando o metodo de alterar logico e ajustes

--- src/main/java/br/com/github/renato28/bytebank/domain/conta/ContaDAO.java
+++ src/main/java/br/com/github/renato28/bytebank/domain/conta/ContaDAO.java
@@ -21,7 +21,7 @@ public class ContaDAO {
 
     public void salvar(DadosAberturaConta dadosDaConta) {
         var cliente = new Cliente(dadosDaConta.dadosCliente());
-        var conta = new Conta(dadosDaConta.numero(), BigDecimal.ZERO, cliente);
+        var conta = new Conta(dadosDaConta.numero(), BigDecimal.ZERO, cliente, true);
 
         String sql = "INSERT INTO conta (numero, saldo, cliente_nome, cliente_cpf, cliente_email)" +
                 "VALUES (?, ?, ?, ?, ?)";
@@ -61,11 +61,12 @@ public class ContaDAO {
                 String nome = resultSet.getString(3);
                 String cpf = resultSet.getString(4);
                 String email = resultSet.getString(5);
+                Boolean estaAtiva = resultSet.getBoolean(6);
 
                 DadosCadastroCliente dadosCadastroCliente = new DadosCadastroCliente(nome,cpf,email);
                 Cliente cliente = new Cliente(dadosCadastroCliente);
 
-                contas.add(new Conta(numero, saldo, cliente));
+                contas.add(new Conta(numero, saldo, cliente, estaAtiva));
             }
             resultSet.close();
             preparedStatement.close();
@@ -94,11 +95,12 @@ public class ContaDAO {
                 String nome = resultSet.getString(3);
                 String cpf = resultSet.getString(4);
                 String email = resultSet.getString(5);
+                Boolean estaAtiva = resultSet.getBoolean(6);
 
                 DadosCadastroCliente dadosCadastroCliente = new DadosCadastroCliente(nome, cpf, email);
                 Cliente cliente = new Cliente(dadosCadastroCliente);
 
-                conta = new Conta(numeroRecuperado, saldo, cliente);
+                conta = new Conta(numeroRecuperado, saldo, cliente, estaAtiva);
             }
 
             resultSet.close();
@@ -136,6 +138,23 @@ public class ContaDAO {
             } catch (SQLException ex) {
                 throw new RuntimeException(ex);
             }
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void alterarLogico(Integer numeroDaConta) {
+        PreparedStatement preparedStatement;
+        String sql = "UPDATE conta SET esta_ativa = false WHERE numero = ?";
+
+        try {
+            preparedStatement = conn.prepareStatement(sql);
+
+            preparedStatement.setInt(1, numeroDaConta);
+
+            preparedStatement.execute();
+            preparedStatement.close();;
+            conn.close();;
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
