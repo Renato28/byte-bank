a23fb893a6cda8ae7242920392b21f4e0a8dc8bf
Renato Nobrega <renatonobrega87@gmail.com>
2026-05-17 17:44:11 -0300

    implementando encerrar conta logico e validacoes

--- src/main/java/br/com/github/renato28/bytebank/domain/conta/ContaService.java
+++ src/main/java/br/com/github/renato28/bytebank/domain/conta/ContaService.java
@@ -5,7 +5,6 @@ import br.com.github.renato28.bytebank.domain.RegraDeNegocioException;
 
 import java.math.BigDecimal;
 import java.sql.Connection;
-import java.util.HashSet;
 import java.util.Set;
 
 public class ContaService {
@@ -15,8 +14,6 @@ public class ContaService {
     public ContaService() {
         this.connection = new ConnectionFactory();
     }
-
-    private Set<Conta> contas = new HashSet<>();
 
     public Set<Conta> listarContasAbertas() {
         Connection conn = connection.recuperarConexao();
@@ -43,6 +40,10 @@ public class ContaService {
             throw new RegraDeNegocioException("Saldo insuficiente!");
         }
 
+        if (!conta.getEstaAtiva()) {
+            throw new RegraDeNegocioException("Conta não está ativa!");
+        }
+
         BigDecimal novoValor = conta.getSaldo().subtract(valor);
         alterar(conta, novoValor);
     }
@@ -51,6 +52,10 @@ public class ContaService {
         var conta = buscarContaPorNumero(numeroDaConta);
         if (valor.compareTo(BigDecimal.ZERO) <= 0) {
             throw new RegraDeNegocioException("Valor do deposito deve ser superior a zero!");
+        }
+
+        if (!conta.getEstaAtiva()) {
+            throw new RegraDeNegocioException("Conta não está ativa!");
         }
 
         BigDecimal novoValor = conta.getSaldo().add(valor);
@@ -71,6 +76,17 @@ public class ContaService {
         Connection conn = connection.recuperarConexao();
 
         new ContaDAO(conn).deletar(numeroDaConta);
+    }
+
+    public void encerrarLogico(Integer numeroDaConta) {
+        var conta = buscarContaPorNumero(numeroDaConta);
+        if (conta.possuiSaldo()) {
+            throw new RegraDeNegocioException("Conta não pode ser encerrada pois ainda possui saldo!");
+        }
+
+        Connection conn = connection.recuperarConexao();
+
+        new ContaDAO(conn).alterarLogico(numeroDaConta);
     }
 
     public Conta buscarContaPorNumero(Integer numero) {
