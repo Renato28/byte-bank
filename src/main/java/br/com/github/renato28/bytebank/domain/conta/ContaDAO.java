f1b4349f8c930a935da49d85253110cf9642efeb
Renato Nobrega <renatonobrega87@gmail.com>
2026-05-17 09:21:16 -0300

    ajustando metodo listar para fechar a conexao

--- src/main/java/br/com/github/renato28/bytebank/domain/conta/ContaDAO.java
+++ src/main/java/br/com/github/renato28/bytebank/domain/conta/ContaDAO.java
@@ -45,13 +45,15 @@ public class ContaDAO {
     }
 
     public Set<Conta> listar() {
+        PreparedStatement preparedStatement;
+        ResultSet resultSet;
         Set<Conta> contas = new HashSet<>();
 
         String sql = "SELECT * FROM conta";
 
         try {
-            PreparedStatement preparedStatement = conn.prepareStatement(sql);
-            ResultSet resultSet = preparedStatement.executeQuery();
+            preparedStatement = conn.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
                 Integer numero = resultSet.getInt(1);
@@ -65,6 +67,9 @@ public class ContaDAO {
 
                 contas.add(new Conta(numero, cliente));
             }
+            resultSet.close();
+            preparedStatement.close();
+            conn.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
