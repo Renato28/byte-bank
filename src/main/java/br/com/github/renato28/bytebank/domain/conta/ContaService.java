dbec9236fc6eb8759f117ebd61152b395f49cc03
Renato Nobrega <renatonobrega87@gmail.com>
2026-05-17 12:29:59 -0300

    refatorando o codigo para usar o metodo de alterar em saque e depositor

--- src/main/java/br/com/github/renato28/bytebank/domain/conta/ContaService.java
+++ src/main/java/br/com/github/renato28/bytebank/domain/conta/ContaService.java
@@ -43,7 +43,8 @@ public class ContaService {
             throw new RegraDeNegocioException("Saldo insuficiente!");
         }
 
-        conta.sacar(valor);
+        BigDecimal novoValor = conta.getSaldo().subtract(valor);
+        alterar(conta, novoValor);
     }
 
     public void realizarDeposito(Integer numeroDaConta, BigDecimal valor) {
@@ -52,7 +53,8 @@ public class ContaService {
             throw new RegraDeNegocioException("Valor do deposito deve ser superior a zero!");
         }
 
-        conta.depositar(valor);
+        BigDecimal novoValor = conta.getSaldo().add(valor);
+        alterar(conta, novoValor);
     }
 
     public void encerrar(Integer numeroDaConta) {
@@ -72,5 +74,10 @@ public class ContaService {
         } else {
             throw new RegraDeNegocioException("Não existe conta cadastrada com esse número!");
         }
+    }
+
+    private void alterar(Conta conta, BigDecimal valor) {
+        Connection conn = connection.recuperarConexao();
+        new ContaDAO(conn).alterar(conta.getNumero(), valor);
     }
 }
